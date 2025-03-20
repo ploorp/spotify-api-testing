da228e8dfc0cf9a9a057a40491bcae43f1448d8c
ploorp <purehtiwf@gmail.com>
2025-03-19 22:24:25 -0400

    get cuurrent song

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import os
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=client_id, 
+    client_secret=client_secret, 
+    scope="user-read-currently-playing"))
+
+def get_current_song():
+    current_song = sp.current_user_playing_track()
+    if current_song:
+        song = current_song['item']['name']
+        artist = current_song['item']['artists'][0]['name']
+        return f"{song} by {artist}"
+    return "no song found"
+
+print(get_current_song())
